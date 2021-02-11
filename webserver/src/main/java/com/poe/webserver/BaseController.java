6ba88c4cdb22d51c8ffc13df9f3d38702b3e3dfd
Ismael J Lopez <ilopez5@hawk.iit.edu>
2021-02-10 20:38:11 -0600

    initial skeleton of all api endpoints added with simple responses

--- webserver/src/main/java/com/poe/webserver/BaseController.java
+++ webserver/src/main/java/com/poe/webserver/BaseController.java
@@ -1,15 +1,16 @@
 package com.poe.webserver;
 
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class BaseController {
+	@GetMapping("/")
+	public String index() {
+		return "Error: Invalid URL";
+	}
 
 	@GetMapping("/api/v1/status")
-	public String index() {
+	public String status() {
 		return "Test Response!";
 	}
 
@@ -18,4 +19,13 @@ public class BaseController {
 		return "Success: " + node.name;
 	}
 
+	@GetMapping("/api/v1/{id}/status")
+	public String nodeStatus(@PathVariable int id) {
+		return "Success: " + id;
+	}
+
+	@GetMapping("/api/v1/broadcast")
+	public String broadcast(@RequestBody BroadcastMessage broadcastMessage) {
+		return "Success: " + broadcastMessage.message;
+	}
 }
