718269c18d77598b88e2c8be5296e9b2d57cc8ec
Avery <averypeckstl@gmail.com>
2021-02-10 20:19:56 -0600

    impl: add post endpoint and Node class

--- webserver/src/main/java/com/poe/webserver/BaseController.java
+++ webserver/src/main/java/com/poe/webserver/BaseController.java
@@ -1,14 +1,21 @@
 package com.poe.webserver;
 
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 public class BaseController {
 
-	@RequestMapping("/")
+	@GetMapping("/api/v1/status")
 	public String index() {
 		return "Test Response!";
+	}
+
+	@PostMapping("/api/v1/add")
+	public String addNode(@RequestBody Node node) {
+		return "Success: " + node.name;
 	}
 
 }
---
+++ webserver/src/main/java/com/poe/webserver/Node.java
@@ -0,0 +1,11 @@
+package com.poe.webserver;
+
+
+public class Node {
+
+    // human readable name describing the node, like "west hallway"
+    public String name;
+    public String macAddress;
+    public String id;
+    public int[] connections;
+}
